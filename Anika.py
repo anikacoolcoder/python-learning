d7d9ce35b0fb8e9d36ac6e07861844c05d8b2a0c
Ankita <Ankita.bhatnagar@outlook.com>
2022-02-21 21:46:15 +0200

    first commit

---
+++ Anika.py
@@ -0,0 +1,10 @@
+from turtle  import*
+
+shape("turtle")
+color("pink")
+for i in range(8):
+    forward(45)
+    backward(45)
+    left(45)
+done()
+
---
+++ home.py
@@ -0,0 +1,13 @@
+from turtle  import*
+
+shape("arrow")
+color("blue")
+for i in range(4):
+    forward(140)
+    right(90)
+left(45)
+forward(100)
+right(90)
+forward(100)
+done()
+
